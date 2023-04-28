5189ef1dcc7769072e8bb44b4d04144d2f886cd8
DropDemBits <r3usrlnd@gmail.com>
2023-04-27 21:14:23 -0400

    remove redundant bindings
    we're manually dispatching to them anyways

--- wdf-kmdf-sys/src/wrapper.c
+++ wdf-kmdf-sys/src/wrapper.c
@@ -1,26 +1,8 @@
 #include "wrapper.h"
 
 void wdf_driver_config_init(
- PWDF_DRIVER_CONFIG        Config,
- PFN_WDF_DRIVER_DEVICE_ADD EvtDriverDeviceAdd
-) {
+    PWDF_DRIVER_CONFIG Config,
+    PFN_WDF_DRIVER_DEVICE_ADD EvtDriverDeviceAdd)
+{
   WDF_DRIVER_CONFIG_INIT(Config, EvtDriverDeviceAdd);
-}
-
-NTSTATUS wdf_driver_create(
-    PDRIVER_OBJECT         DriverObject,
-    PCUNICODE_STRING       RegistryPath,
-    PWDF_OBJECT_ATTRIBUTES DriverAttributes,
-    PWDF_DRIVER_CONFIG     DriverConfig,
-   WDFDRIVER              *Driver
-) {
- return WdfDriverCreate(DriverObject, RegistryPath, DriverAttributes, DriverConfig, Driver); 
-}
-
-NTSTATUS wdf_device_create(
-  PWDFDEVICE_INIT        *DeviceInit,
-  PWDF_OBJECT_ATTRIBUTES DeviceAttributes,
-  WDFDEVICE              *Device
-) {
- return WdfDeviceCreate(DeviceInit, DeviceAttributes, Device);
 }
