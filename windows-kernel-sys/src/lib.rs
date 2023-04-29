0438be47890d00dc079efd1dbe98844ce8c82980
DropDemBits <r3usrlnd@gmail.com>
2023-04-29 03:32:06 -0400

    Add `Error` and windows-sys reexports
    also shunts generated bindings into a separate file for
    ✨ better incrementality ✨

--- windows-kernel-sys/src/lib.rs
+++ windows-kernel-sys/src/lib.rs
@@ -7,4 +7,24 @@
     clippy::missing_safety_doc,
 )]
 
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+mod bindings;
+
+pub use bindings::*;
+pub use windows_sys as sys;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+pub struct Error(pub NTSTATUS);
+
+impl Error {
+    /// Converts an [`NTSTATUS`] into an `Error`.
+    ///
+    /// Note that this treats anything that isn't `STATUS_SUCCESSFUL` as an error
+    pub fn to_err(status: NTSTATUS) -> Result<(), Error> {
+        if status == 0 {
+            Ok(())
+        } else {
+            Err(Error(status))
+        }
+    }
+}
