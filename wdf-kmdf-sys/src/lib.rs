2b91fae8c40ace477e9e90d99881fe0b591c43b8
DropDemBits <r3usrlnd@gmail.com>
2023-05-25 17:01:57 -0400

    stick `init` assoc fn on `WDF_FILEOBJECT_CONFIG`
    will probably be the way to port all future default struct init
    inline methods

--- wdf-kmdf-sys/src/lib.rs
+++ wdf-kmdf-sys/src/lib.rs
@@ -49,3 +49,36 @@ extern "C" {
         EvtDriverDeviceAdd: PFN_WDF_DRIVER_DEVICE_ADD,
     );
 }
+
+// Right now, we don't handle struct versioning, so it's just the struct's size.
+// Should probably be a `Result<u32, Error>`
+fn WDF_STRUCTURE_SIZE<T: Sized>() -> u32 {
+    core::mem::size_of::<T> as u32
+}
+
+impl WDF_FILEOBJECT_CONFIG {
+    /// Sets:
+    /// - Size
+    /// - Specified callback function pointers
+    /// - FileObjectClass to [`_WDF_FILEOBJECT_CLASS::WdfFileObjectWdfCannotUseFsContexts`]
+    /// - AutoForwardCleanupClose to [`_WDF_TRI_STATE::WdfUseDefault`]
+    pub fn init(
+        EvtDeviceFileCreate: PFN_WDF_DEVICE_FILE_CREATE, // in, optional
+        EvtFileClose: PFN_WDF_FILE_CLOSE,                // in, optional
+        EvtFileCleanup: PFN_WDF_FILE_CLEANUP,            // in, optional
+    ) -> Self {
+        // SAFETY: All are zero-able
+        let mut config: Self = unsafe { core::mem::zeroed() };
+
+        config.Size = WDF_STRUCTURE_SIZE::<Self>();
+
+        config.EvtDeviceFileCreate = EvtDeviceFileCreate;
+        config.EvtFileClose = EvtFileClose;
+        config.EvtFileCleanup = EvtFileCleanup;
+
+        config.FileObjectClass = _WDF_FILEOBJECT_CLASS::WdfFileObjectWdfCannotUseFsContexts;
+        config.AutoForwardCleanupClose = _WDF_TRI_STATE::WdfUseDefault;
+
+        config
+    }
+}
