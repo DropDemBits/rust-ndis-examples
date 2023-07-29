aca7d1e08e064fb37bdb9130f3b4f9be9fdd3e13
DropDemBits <r3usrlnd@gmail.com>
2023-07-29 17:27:24 -0400

    make an adapter and stub out protocol charateristics functions

--- ndisprot-kmdf/src/lib.rs
+++ ndisprot-kmdf/src/lib.rs
@@ -1,16 +1,23 @@
 #![no_std]
-#![feature(allocator_api, const_option, result_option_inspect)]
+#![allow(non_snake_case, unused_variables, dead_code)] // Cut down on the warnings for now
+#![feature(allocator_api, const_option, result_option_inspect, offset_of)]
+
+use core::mem::MaybeUninit;
 
 use wdf_kmdf_sys::{
     WDFDEVICE, WDFFILEOBJECT, WDFQUEUE, WDFREQUEST, WDF_FILEOBJECT_CONFIG, WDF_IO_QUEUE_CONFIG,
-    WDF_IO_QUEUE_DISPATCH_TYPE, WDF_NO_OBJECT_ATTRIBUTES,
+    WDF_IO_QUEUE_DISPATCH_TYPE,
 };
 use windows_kernel_rs::{
-    log::{self, debug, error, info, trace},
+    log::{self, debug, error},
     string::{utf16str, UnicodeString},
     DriverObject,
 };
-use windows_kernel_sys::{Error, NTSTATUS, PDRIVER_OBJECT, PUNICODE_STRING, ULONG};
+use windows_kernel_sys::{
+    Error, NDIS_HANDLE, NDIS_OBJECT_TYPE_PROTOCOL_DRIVER_CHARACTERISTICS,
+    NDIS_PROTOCOL_DRIVER_CHARACTERISTICS, NDIS_PROTOCOL_DRIVER_CHARACTERISTICS_REVISION_1,
+    NTSTATUS, PDRIVER_OBJECT, PUNICODE_STRING, ULONG,
+};
 
 #[allow(non_snake_case)]
 #[no_mangle]
@@ -40,6 +47,7 @@ fn driver_entry(
     driver_object: DriverObject,
     registry_path: UnicodeString<'_>,
 ) -> Result<(), Error> {
+    const PROTO_NAME: UnicodeString<'static> = UnicodeString::new_const(utf16str!("NDISPROT"));
     debug!("DriverEntry");
 
     wdf_kmdf::driver::Driver::<NdisProt>::create(
@@ -70,6 +78,57 @@ fn driver_entry(
                 let control_device = create_control_device(driver, p_init).inspect_err(|err| {
                     error!("create_control_device failed with status {:#x}", err.0)
                 })?;
+
+                const NDIS_SIZEOF_PROTOCOL_DRIVER_CHARACTERISTICS_REVISION_1: u16 =
+                    (core::mem::offset_of!(
+                        NDIS_PROTOCOL_DRIVER_CHARACTERISTICS,
+                        SendNetBufferListsCompleteHandler
+                    ) + core::mem::size_of::<
+                        windows_kernel_sys::SEND_NET_BUFFER_LISTS_COMPLETE_HANDLER,
+                    >()) as u16;
+
+                // initialize the protocol characteristic structure
+                let mut proto_char =
+                    unsafe { core::mem::zeroed::<NDIS_PROTOCOL_DRIVER_CHARACTERISTICS>() };
+
+                proto_char.Header.Type = NDIS_OBJECT_TYPE_PROTOCOL_DRIVER_CHARACTERISTICS as u8;
+                proto_char.Header.Size = NDIS_SIZEOF_PROTOCOL_DRIVER_CHARACTERISTICS_REVISION_1;
+                proto_char.Header.Revision = NDIS_PROTOCOL_DRIVER_CHARACTERISTICS_REVISION_1 as u8;
+
+                proto_char.MajorNdisVersion = 6;
+                proto_char.MinorNdisVersion = 0;
+                proto_char.Name = unsafe { PROTO_NAME.into_raw() };
+
+                proto_char.SetOptionsHandler = None;
+                proto_char.OpenAdapterCompleteHandlerEx = Some(ndisbind::open_adapter_complete);
+                proto_char.CloseAdapterCompleteHandlerEx = Some(ndisbind::close_adapter_complete);
+                proto_char.BindAdapterHandlerEx = Some(ndisbind::bind_adapter);
+                proto_char.UnbindAdapterHandlerEx = Some(ndisbind::unbind_adapter);
+                proto_char.OidRequestCompleteHandler = Some(ndisbind::request_complete);
+                proto_char.StatusHandlerEx = Some(ndisbind::status_handler);
+                proto_char.NetPnPEventHandler = Some(ndisbind::pnp_event_handler);
+                proto_char.UninstallHandler = None;
+
+                proto_char.SendNetBufferListsCompleteHandler = Some(send::send_complete);
+                proto_char.ReceiveNetBufferListsHandler = Some(recv::receive_net_buffer_lists);
+
+                // Register as a protocol driver
+                let mut handle = MaybeUninit::<NDIS_HANDLE>::uninit();
+                let status = Error::to_err(unsafe {
+                    windows_kernel_sys::NdisRegisterProtocolDriver(
+                        core::ptr::null_mut(),
+                        &mut proto_char,
+                        handle.as_mut_ptr(),
+                    )
+                });
+
+                if let Err(err) = status {
+                    windows_kernel_rs::log::warn!(
+                        "Failed to register protocol driver with NDIS (status code {:#x?})",
+                        err.0
+                    );
+                    return Err(err);
+                }
 
                 Ok(pinned_init::try_init!(NdisProt {
                     control_device: control_device,
@@ -291,4 +350,111 @@ pub extern "system" fn __CxxFrameHandler3() -> i32 {
 #[panic_handler]
 fn panic(info: &core::panic::PanicInfo) -> ! {
     windows_kernel_rs::__handle_panic(info);
+}
+
+mod ndisbind {
+    //! NDIS protocol entry points as well as handling binding and unbinding from adapters
+    use windows_kernel_sys::{
+        sys::Win32::Foundation::STATUS_SUCCESS, NDIS_HANDLE, NDIS_OID, NTSTATUS,
+        OID_802_11_ADD_WEP, OID_802_11_AUTHENTICATION_MODE, OID_802_11_BSSID,
+        OID_802_11_BSSID_LIST, OID_802_11_BSSID_LIST_SCAN, OID_802_11_CONFIGURATION,
+        OID_802_11_DISASSOCIATE, OID_802_11_INFRASTRUCTURE_MODE, OID_802_11_NETWORK_TYPE_IN_USE,
+        OID_802_11_POWER_MODE, OID_802_11_RELOAD_DEFAULTS, OID_802_11_REMOVE_WEP, OID_802_11_RSSI,
+        OID_802_11_SSID, OID_802_11_STATISTICS, OID_802_11_SUPPORTED_RATES, OID_802_11_WEP_STATUS,
+        OID_802_3_MULTICAST_LIST, PNDIS_BIND_PARAMETERS, PNDIS_OID_REQUEST,
+        PNDIS_STATUS_INDICATION, PNET_PNP_EVENT_NOTIFICATION,
+    };
+
+    const SUPPORTED_SET_OIDS: &[NDIS_OID] = &[
+        OID_802_11_INFRASTRUCTURE_MODE,
+        OID_802_11_AUTHENTICATION_MODE,
+        OID_802_11_RELOAD_DEFAULTS,
+        OID_802_11_REMOVE_WEP,
+        OID_802_11_WEP_STATUS,
+        OID_802_11_BSSID_LIST_SCAN,
+        OID_802_11_ADD_WEP,
+        OID_802_11_SSID,
+        OID_802_11_BSSID,
+        OID_802_11_BSSID_LIST,
+        OID_802_11_DISASSOCIATE,
+        // the following two are apparently used by power management?
+        OID_802_11_STATISTICS,
+        OID_802_11_POWER_MODE,
+        OID_802_11_NETWORK_TYPE_IN_USE,
+        OID_802_11_RSSI,
+        OID_802_11_SUPPORTED_RATES,
+        OID_802_11_CONFIGURATION,
+        OID_802_3_MULTICAST_LIST,
+    ];
+
+    pub(crate) unsafe extern "C" fn bind_adapter(
+        ProtocolDriverContext: NDIS_HANDLE,
+        BindContext: NDIS_HANDLE,
+        BindParameters: PNDIS_BIND_PARAMETERS,
+    ) -> NTSTATUS {
+        STATUS_SUCCESS
+    }
+
+    pub(crate) unsafe extern "C" fn open_adapter_complete(
+        ProtocolBindingContext: NDIS_HANDLE,
+        Status: NTSTATUS,
+    ) {
+    }
+
+    pub(crate) unsafe extern "C" fn unbind_adapter(
+        UnbindContext: NDIS_HANDLE,
+        ProtocolBindingContext: NDIS_HANDLE,
+    ) -> NTSTATUS {
+        STATUS_SUCCESS
+    }
+
+    pub(crate) unsafe extern "C" fn close_adapter_complete(ProtocolBindingContext: NDIS_HANDLE) {}
+
+    pub(crate) unsafe extern "C" fn pnp_event_handler(
+        ProtocolBindingContext: NDIS_HANDLE,
+        NetPnPEventNotification: PNET_PNP_EVENT_NOTIFICATION,
+    ) -> NTSTATUS {
+        STATUS_SUCCESS
+    }
+
+    pub(crate) unsafe extern "C" fn NdisprotProtocolUnloadHandler() -> NTSTATUS {
+        STATUS_SUCCESS
+    }
+
+    pub(crate) unsafe extern "C" fn request_complete(
+        ProtocolBindingContext: NDIS_HANDLE,
+        OidRequest: PNDIS_OID_REQUEST,
+        Status: NTSTATUS,
+    ) {
+    }
+
+    pub(crate) unsafe extern "C" fn status_handler(
+        ProtocolBindingContext: NDIS_HANDLE,
+        StatusIndication: PNDIS_STATUS_INDICATION,
+    ) {
+    }
+}
+
+mod send {
+    use windows_kernel_sys::{NDIS_HANDLE, PNET_BUFFER_LIST, ULONG};
+
+    pub(crate) unsafe extern "C" fn send_complete(
+        ProtocolBindingContext: NDIS_HANDLE,
+        NetBufferList: PNET_BUFFER_LIST,
+        SendCompleteFlags: ULONG,
+    ) {
+    }
+}
+
+mod recv {
+    use windows_kernel_sys::{NDIS_HANDLE, NDIS_PORT_NUMBER, PNET_BUFFER_LIST, ULONG};
+
+    pub(crate) unsafe extern "C" fn receive_net_buffer_lists(
+        ProtocolBindingContext: NDIS_HANDLE,
+        NetBufferLists: PNET_BUFFER_LIST,
+        PortNumber: NDIS_PORT_NUMBER,
+        NumberOfNetBufferLists: ULONG,
+        ReceiveFlags: ULONG,
+    ) {
+    }
 }
