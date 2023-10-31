6dcad8a22d580ae30f506dd388f499932e26454e
DropDemBits <r3usrlnd@gmail.com>
2023-10-31 01:40:39 -0400

    Make `FileObject` ref-countable

--- driver/wdf-kmdf/src/file_object.rs
+++ driver/wdf-kmdf/src/file_object.rs
@@ -4,13 +4,23 @@ use pinned_init::PinInit;
 use wdf_kmdf_sys::WDFFILEOBJECT;
 use windows_kernel_sys::Error;
 
-use crate::object::{self, IntoContextSpace};
+use crate::{
+    object::{self, HandleKind, IntoContextSpace},
+    raw,
+};
 
-pub struct FileObject<T>(WDFFILEOBJECT, PhantomData<T>);
+pub struct FileObject<T> {
+    handle: WDFFILEOBJECT,
+    kind: HandleKind,
+    _context: PhantomData<T>,
+}
 
 impl<T> core::fmt::Debug for FileObject<T> {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        f.debug_tuple("FileObject").field(&self.0).finish()
+        f.debug_struct("FileObject")
+            .field("handle", &self.handle)
+            .field("kind", &self.kind)
+            .finish()
     }
 }
 
@@ -24,12 +34,16 @@ impl<T> FileObject<T> {
     /// Also, the context space must be initialized.
     // FIXME: Make a proper wrapper eventually
     pub unsafe fn wrap(handle: WDFFILEOBJECT) -> Self {
-        Self(handle, PhantomData)
+        Self {
+            handle,
+            kind: HandleKind::Wrapped,
+            _context: PhantomData,
+        }
     }
 
     /// Gets the file object handle for use with WDF functions that don't have clean wrappers yet
     pub fn raw_handle(&mut self) -> WDFFILEOBJECT {
-        self.0
+        self.handle
     }
 }
 
@@ -52,14 +66,56 @@ where
         // Caller guarantees that this is only called once
         unsafe { object::context_pin_init(self, |_| Ok(init_context)) }
     }
+
+    /// Makes a shared reference to the file object
+    ///
+    /// ## IRQL: <= Dispatch
+    pub fn clone_ref(&self) -> FileObject<T> {
+        // Safety: Caller ensures that we're at the right IRQL
+        unsafe {
+            raw::WdfObjectReferenceActual(
+                self.handle.cast(),
+                None,
+                line!() as i32,
+                Some(cstr!(file!()).as_ptr()),
+            )
+        }
+
+        FileObject {
+            kind: HandleKind::Ref,
+            ..*self
+        }
+    }
+}
+
+impl<T> Drop for FileObject<T> {
+    fn drop(&mut self) {
+        // FIXME: Assert that this is at <= DISPATCH_LEVEL
+        match self.kind {
+            HandleKind::Wrapped => {}
+            // Safety: assertion that we're at the correct IRQL
+            HandleKind::Owned | HandleKind::Parented => unsafe {
+                raw::WdfObjectDelete(self.handle.cast())
+            },
+            // Safety: assertion that we're at the correct IRQL
+            HandleKind::Ref => unsafe {
+                raw::WdfObjectDereferenceActual(
+                    self.handle.cast(),
+                    None,
+                    line!() as i32,
+                    Some(cstr!(file!()).as_ptr()),
+                )
+            },
+        }
+    }
 }
 
 impl<T> object::AsObjectHandle for FileObject<T> {
     fn as_handle(&self) -> wdf_kmdf_sys::WDFOBJECT {
-        self.0.cast()
+        self.handle.cast()
     }
 
     fn as_handle_mut(&mut self) -> wdf_kmdf_sys::WDFOBJECT {
-        self.0.cast()
+        self.handle.cast()
     }
 }
