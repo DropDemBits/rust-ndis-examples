3c4ada787da9e80e37025f5a83f812e09b87c0cd
DropDemBits <r3usrlnd@gmail.com>
2023-12-25 13:57:21 -0500

    Move linker flags into the build script
    Allows for testing `ndis-rs` while also still correctly compiling the driver.

--- driver/ndisprot-kmdf/build.rs
+++ driver/ndisprot-kmdf/build.rs
@@ -90,4 +90,24 @@ fn main() {
     println!("cargo:rustc-link-lib=wdfdriverentry");
 
     println!("cargo:rustc-link-lib=ndis");
+
+    // Emit link flags
+    let link_flags = &[
+        "/NOLOGO",
+        "/NXCOMPAT",
+        "/NODEFAULTLIB",
+        "/SUBSYSTEM:NATIVE",
+        "/DRIVER",
+        "/DYNAMICBASE",
+        "/MANIFEST:NO",
+        "/OPT:REF,ICF",
+        // See <https://github.com/SergiusTheBest/FindWDK/blob/c941028b26565f756a16ea815d51ac9781f00e79/cmake/FindWdk.cmakeL174>
+        "/ENTRY:FxDriverEntry",
+        "/MERGE:.edata=.rdata",
+        "/MERGE:.rustc=.data",
+        "/INTEGRITYCHECK",
+    ];
+    for flag in link_flags {
+        println!("cargo:rustc-cdylib-link-arg={flag}");
+    }
 }
